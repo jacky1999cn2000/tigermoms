8b19fe847d35dd6a3bdf928b2ca674d33bc59a0a
Jacky Zhao <jacky.zhao@bunchball.com>
2016-06-20 19:40:35 +0800

    add react-native-vector-icons

--- app/components/Navigator/config/routeMap.js
+++ app/components/Navigator/config/routeMap.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import SignIn from '../../../components/SignInSignUp/SignIn';
+import SignUp from '../../../components/SignInSignUp/SignUp';
 import App from '../../../components/App/App';
+
 // import Detail from '../components/Detail'
 // import NewItem from '../components/NewItem'
 
 module.exports = {
   signin: SignIn,
+  signup: SignUp,
   app: App,
   // detail: Detail,
   // newItem: NewItem
